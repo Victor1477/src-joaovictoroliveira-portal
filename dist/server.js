97ffeaf7554bed255ccc6e915bc85fbb9c42c68f
Joao Oliveira <victorjoao129@hotmail.com>
2024-05-30 14:40:47 -0300

    adding logger

--- dist/server.js
+++ dist/server.js
@@ -4,13 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const logger_1 = require("./app/logger");
 console.clear();
 const app = (0, express_1.default)();
+const logger = new logger_1.Logger();
 var ServerConfig;
 (function (ServerConfig) {
     ServerConfig[ServerConfig["PORT"] = 8080] = "PORT";
     ServerConfig["DIST"] = "/opt/projects/src-joaovictoroliveira-portal/dist/app";
 })(ServerConfig || (ServerConfig = {}));
+app.use("/application/logs", (req, res, next) => {
+    logger.plus(req.originalUrl);
+    res.send(logger.results);
+});
+app.use("/", (req, res, next) => {
+    logger.plus(req.originalUrl);
+    next();
+});
 app.use("/", express_1.default.static(ServerConfig.DIST));
 app.use("", (req, res, next) => {
     res.sendFile(ServerConfig.DIST + "/index.html");
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,24 @@
 import express from "express";
+import { Logger } from "./app/logger";
 
 console.clear();
 const app = express();
+const logger = new Logger();
 
 enum ServerConfig {
   PORT = 8080,
   DIST = "/opt/projects/src-joaovictoroliveira-portal/dist/app",
 }
+
+app.use("/application/logs", (req, res, next) => {
+  logger.plus(req.originalUrl);
+  res.send(logger.results);
+});
+
+app.use("/", (req, res, next) => {
+  logger.plus(req.originalUrl);
+  next();
+});
 
 app.use("/", express.static(ServerConfig.DIST));
 
