1e6aa562636336fed3e241bf690fe89690cd139a
Joao Victor Silva De Oliveira <joao.soliveira@telefonica.com>
2024-11-05 21:43:04 -0300

    adding logger unit test

--- testsrc/app/logger.spec.ts
+++ testsrc/app/logger.spec.ts
@@ -9,7 +9,7 @@ beforeEach(() => {
 
 it("Should instanciate logger with the results defined", () => {
   const results = logger.results;
-  expect(results.startDate).not.toBeNull();
+  expect(results.startDate).match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/);
   expect(results.requestCounter).toEqual({});
 });
 
